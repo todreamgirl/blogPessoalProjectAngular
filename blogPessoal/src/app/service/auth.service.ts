c5921de820a987b18c68392cada9c51cf3aa6cb4
unknown <jacquelinee.barbosa@hotmail.com>
2020-12-04 15:20:41 -0300

    authServiceCadastroLoginSenha

--- blogPessoal/src/app/service/auth.service.ts
+++ blogPessoal/src/app/service/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -9,10 +10,30 @@ import { UserLogin } from '../model/UserLogin';
 export class AuthService {
 
   constructor(private http: HttpClient) { }
+
   logar(userLogin: UserLogin){
-    return this.http.post('http://localhost:9000/usuarios/logar', userLogin)
+    return this.http.post<UserLogin>('http://localhost:9000/usuarios/logar', userLogin)
   }
-  cadastrar(user: User){
-    return this.http.post('http://localhost:9000/usuarios/cadastrar', User)
+  cadastrar(user: User): Observable<User>{
+    return this.http.post<User>('http://localhost:9000/usuarios/cadastrar', user)
+  }
+  btnSair(){
+    let ok=false
+    let token=localStorage.getItem('token')
+    
+    if(token != null){
+      ok=true
+    }
+    return ok
+  }
+
+  btnLogin(){
+    let ok=false
+    let token=localStorage.getItem('token')
+    
+    if(token == null){
+      ok=true
+    }
+    return ok
   }
 }
